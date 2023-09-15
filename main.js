7a1df6edafc422f2cfa58d5509dea6173ea1849c
amandeep-patro <amandeep.patro04@gmail.com>
2023-09-15 13:50:18 +0530

    login-2

--- main.js
+++ main.js
@@ -1,22 +1,3 @@
-const container__nav = document.querySelector(".container__nav");
-const primaryNav = document.querySelector(".nav__list");
-const toggleButton = document.querySelector(".nav-toggle");
-
-toggleButton.addEventListener("click", () => {
-    const isExpanded = primaryNav.getAttribute("aria-expanded");
-    primaryNav.setAttribute(
-        "aria-expanded",
-        isExpanded === "false" ? "true" : "false"
-    );
-});
-
-container__nav.addEventListener("click", (e) => {
-    if (!primaryNav.contains(e.target) && !toggleButton.contains(e.target)) {
-        primaryNav.setAttribute("aria-expanded", "false");
-    }
-});
-
-
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
